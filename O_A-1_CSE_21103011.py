1b52e0046845e6740508b8716b12ba2c64149637
Sanchita Goel <97080446+Sanchita2643@users.noreply.github.com>
2022-01-14 23:18:52 +0530

    Add files via upload

---
+++ O_A-1_CSE_21103011.py
@@ -0,0 +1,35 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+====================== RESTART: C:/Users/Sanchita/Ques1.py =====================
+Enter first number:25
+Enter second number:62
+Enter third number:21
+Average of the three numbers is : 36.0
+
+
+====================== RESTART: C:/Users/Sanchita/Ques2.py =====================
+enter your gross income: 1500000
+Enter your number of dependents: 2000
+Your income tax is : 297000.0
+
+====================== RESTART: C:/Users/Sanchita/Ques3.py =====================
+Enter your SID:21103011
+Enter your name:Sanchita Goel
+Enter your Gender:Female
+Enter your course name:CSE
+Enter your CGPA:9.8
+['21103011', 'Sanchita Goel', 'Female', 'CSE', 9.8]
+
+====================== RESTART: C:/Users/Sanchita/Ques4.py =====================
+Enter marks of students: 19
+Enter marks of students: 15
+Enter marks of students: 17
+Enter marks of students: 14
+Enter marks of students: 18
+['14', '15', '17', '18', '19']
+
+
+====================== RESTART: C:/Users/Sanchita/Ques5.py =====================
+Part a question :  ['Red', 'Green', 'White', 'Pink', 'Yellow']
+Part b of question : ['Red', 'Green', 'White', 'Purple', 'Yellow']
