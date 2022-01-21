d291bd1a390cd6e7c9bb2a7fc3af9e254a4bfcfa
Sanchita Goel <97080446+Sanchita2643@users.noreply.github.com>
2022-01-21 17:18:46 +0530

    Add files via upload

---
+++ Output_Assign-2_21103011.py
@@ -0,0 +1,52 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============ RESTART: C:/Users/Sanchita/OneDrive/Documents/Ques1.py ============
+<A>THE LENGTH OF STRING IS 35
+<B>THE REVERSED STRING IS  egaugnal evitisnes esac a si nohtyP
+<E>THE INDEX OF SUBSTRING a is  10
+<F>THE ORIGINAL STRING AFTER REMOVING WHITESPACES IS Pythonisacasesensitivelanguage
+
+
+
+======== RESTART: C:/Users/Sanchita/OneDrive/Documents/Ques2.py ========
+ENTER YOUR NAME Sanchita Goel
+ENTER YOUR SID 21103011
+ENTER YOUR DEPARTMENT CSE
+ENTER YOUR CGPA 9.8
+Hey  Sanchita Goel, Here!
+My SID is 21103011
+I am from  CSE and my CGPA is 9.800000
+
+
+
+======== RESTART: C:/Users/Sanchita/OneDrive/Documents/Ques3.py ========
+a.  8
+b.  58
+c.  50
+Left shift both a and b with 2 bits respectively are : 224 40
+Right shift a with 2 bits and b with 4 bits respectively are :  14 0
+
+
+
+======== RESTART: C:/Users/Sanchita/OneDrive/Documents/Ques4.py ========
+Enter first number: 41
+Enter second number: 78
+Enter third number: 24
+THE LARGEST NUMBER IS  78
+
+
+
+======== RESTART: C:/Users/Sanchita/OneDrive/Documents/Ques5.py ========
+ENTER A STRING Sanchita
+No
+
+
+
+======== RESTART: C:/Users/Sanchita/OneDrive/Documents/Ques6.py ========
+ENTER FIRST SIDE OF TRIANGLE 54
+ENTER SECOND SIDE OF TRIANGLE34
+ENTER THIRD SIDE OF TRIANGLE84
+Yes
+
+
