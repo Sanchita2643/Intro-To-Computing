ee54fd65dad77ebf163db1421abf6b9461e16751
Sanchita Goel <97080446+Sanchita2643@users.noreply.github.com>
2022-02-14 13:38:40 +0530

    Add files via upload

---
+++ Output_Assign-3_CSE_21103011.py
@@ -0,0 +1,109 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============== RESTART: C:/Users/Sanchita/OneDrive/Documents/Q1.py =============
+Give a string-adfgrhijjdhrgaa
+{'a': 3, 'd': 2, 'f': 1, 'g': 2, 'r': 2, 'h': 2, 'i': 1, 'j': 2}
+
+========== RESTART: C:/Users/Sanchita/OneDrive/Documents/Q2.py =========
+Give month-4
+Give day-25
+Give year-2005
+Date- 25 / 4 / 2005
+Next Date- 26 / 4 / 2005
+
+========== RESTART: C:/Users/Sanchita/OneDrive/Documents/Q3.py =========
+[(1, 1), (2, 4), (3, 9), (4, 16), (5, 25), (6, 36), (7, 49), (8, 64), (9, 81), (10, 100)]
+
+========== RESTART: C:/Users/Sanchita/OneDrive/Documents/Q4.py =========
+Give a number between 4 and 10 including them-8
+Performance=Very Good
+Letter Grade=B+
+
+========== RESTART: C:/Users/Sanchita/OneDrive/Documents/Q5.py =========
+ ABCDEFGHIJK
+  ABCDEFGHI
+   ABCDEFG
+    ABCDE
+     ABC
+      A
+
+========== RESTART: C:/Users/Sanchita/OneDrive/Documents/Q6.py =========
+----------------(a)-------------------
+Give the sid (a number):4
+Enter your name:s
+give a letter y or n:y
+Give the sid (a number):6
+Enter your name:a
+give a letter y or n:y
+Give the sid (a number):2
+Enter your name:r
+give a letter y or n:y
+Give the sid (a number):1
+Enter your name:g
+give a letter y or n:y
+Give the sid (a number):3
+Enter your name:h
+give a letter y or n:n
+The required dictionary-- {4: 's', 6: 'a', 2: 'r', 1: 'g', 3: 'h'}
+The list will be used in further parts--
+The required list-- [(4, 's'), (6, 'a'), (2, 'r'), (1, 'g'), (3, 'h')]
+----------------(b)------------------------
+The dictionary we had-
+{4: 's', 6: 'a', 2: 'r', 1: 'g', 3: 'h'}
+now we have exchanged the key value pair but its not sorted-
+{'s': 4, 'a': 6, 'r': 2, 'g': 1, 'h': 3}
+The unsorted list--
+[('s', 4), ('a', 6), ('r', 2), ('g', 1), ('h', 3)]
+The sorted list--
+[('a', 6), ('g', 1), ('h', 3), ('r', 2), ('s', 4)]
+The sorted dictionary-
+{'a': 6, 'g': 1, 'h': 3, 'r': 2, 's': 4}
+This is the dictionary sorted on the basis of name-
+{6: 'a', 1: 'g', 3: 'h', 2: 'r', 4: 's'}
+-----------------(c)-----------------
+The list we made before will be used now-
+[(4, 's'), (6, 'a'), (2, 'r'), (1, 'g'), (3, 'h')]
+now the list is sorted-
+[(1, 'g'), (2, 'r'), (3, 'h'), (4, 's'), (6, 'a')]
+sorted dictionary based on sid- {1: 'g', 2: 'r', 3: 'h', 4: 's', 6: 'a'}
+-----------------(d)-----------------
+Please enter one of the sids you entered before-4
+The name of the student with the entered sid is-
+s
+
+========== RESTART: C:/Users/Sanchita/OneDrive/Documents/Q7.py =========
+Give a number-5
+Give a number-7
+[5, 7, 12]
+Give y to contnue and n to stop going further-y
+[5, 7, 12, 19]
+Give y to contnue and n to stop going further-y
+[5, 7, 12, 19, 31]
+Give y to contnue and n to stop going further-y
+[5, 7, 12, 19, 31, 50]
+Give y to contnue and n to stop going further-y
+[5, 7, 12, 19, 31, 50, 81]
+Give y to contnue and n to stop going further-y
+[5, 7, 12, 19, 31, 50, 81, 131]
+Give y to contnue and n to stop going further-y
+[5, 7, 12, 19, 31, 50, 81, 131, 212]
+Give y to contnue and n to stop going further-y
+[5, 7, 12, 19, 31, 50, 81, 131, 212, 343]
+Give y to contnue and n to stop going further-n
+Now we got a list having a fibonacci series-
+[5, 7, 12, 19, 31, 50, 81, 131, 212, 343]
+Average of the sequence-
+89.1
+
+========== RESTART: C:/Users/Sanchita/OneDrive/Documents/Q8.py =========
+-------------(a)------------
+{1, 3, 5, 6, 8}
+-------------(b)------------
+{3, 6, 8, 9, 13, 17}
+-------------(c)------------
+{1, 2, 4, 5}
+-------------(d)------------
+{6, 7, 8, 9, 10}
+-------------(e)------------
+{10, 7}
